aeb5437096eee199e302486043da1ba429e00bee
Jack Hagen <jjhags@gmail.com>
2025-10-15 09:40:39 -0500

    Created the base structure of the code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CS370-StudentInformationSystemSecurity
-Our project for CS370, computer security
+
+Our final for CS370, computer security is to build a simple student database web server with basic security controls
+
+For our web server we will use a framework called Flask, which is a standard web server program that is simple to use for the purposes of this assignment. We will use Flask for all of the Graphic User Interface requirements. Flask will contain the software that allows users to edit the appropriate data. The actual data will be stored in a SQLite which has the functionality of a SQL database, but stored conveniently in a single document. The SQLite document will store all user information including login credentials, and student information.
+
+SQLlite as database (editable document that is easier to manipulate than MySQL)
+Flask as frontend software (similar to Python) 
---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = 'secret_key_ihgwkg86fwge7gwiy3efefef23fe3f'
+
+DATABASE = 'database.sql'
+
+def create_database():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS login (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role TEXT NOT NULL
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            address TEXT NOT NULL,
+            info TEXT,
+            credits INTEGER DEFAULT 0
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS faculty (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            info TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    # Render homepage
+    pass
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Handle user login
+    pass
+
+@app.route('/logout')
+def logout():
+    # Handle user logout
+    pass
+
+@app.route('/new_account', methods=['GET', 'POST'])
+def new_account():
+    # Handle new account creation
+    pass
+
+@app.route('/student_edit', methods=['GET', 'POST'])
+def student_edit():
+    # Edit student information
+    pass
+
+@app.route('/faculty_update', methods=['GET', 'POST'])
+def faculty_update():
+    # Edit faculty information
+    pass
+
+create_database()
+if __name__ == '__main__':
+    app.run(debug=True)
+
